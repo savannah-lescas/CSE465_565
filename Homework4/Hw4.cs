a7f8ed9478f5b418e6e14cb31a49c0fe23350e41
savannah-lescas <lescas6502@gmail.com>
2024-04-02 20:48:45 -0400

    added the files to the new Homework4 folder

---
+++ Homework4/Hw4.cs
@@ -0,0 +1,56 @@
+/* 
+  Homework#4
+
+  Add your name here: Savannah Lescas
+
+  You are free to create as many classes within the Hw4.cs file or across 
+  multiple files as you need. However, ensure that the Hw4.cs file is the 
+  only one that contains a Main method. This method should be within a 
+  class named hw4. This specific setup is crucial because your instructor 
+  will use the hw4 class to execute and evaluate your work.
+  */
+  // BONUS POINT:
+  // => Used Pointers from lines 10 to 15 <=
+  // => Used Pointers from lines 40 to 63 <=
+  
+
+using System;
+
+public class Hw4
+{
+    public static void Main(string[] args)
+    {
+        // Capture the start time
+        // Must be the first line of this method
+        DateTime startTime = DateTime.Now; // Do not change
+        // ============================
+        // Do not add or change anything above, inside the 
+        // Main method
+        // ============================
+
+
+
+
+
+        // TODO: your code goes here
+
+
+
+
+        
+
+        // ============================
+        // Do not add or change anything below, inside the 
+        // Main method
+        // ============================
+
+        // Capture the end time
+        DateTime endTime = DateTime.Now;  // Do not change
+        
+        // Calculate the elapsed time
+        TimeSpan elapsedTime = endTime - startTime; // Do not change
+        
+        // Display the elapsed time in milliseconds
+        Console.WriteLine($"Elapsed Time: {elapsedTime.TotalMilliseconds} ms");
+    }
+}
