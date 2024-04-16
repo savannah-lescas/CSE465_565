edfedd9fe99390b85d8f53291739c6e7e4bfaead
savannah-lescas <lescas6502@gmail.com>
2024-04-16 19:38:48 -0400

    Added initial files

---
+++ Homework5/hw5.py
@@ -0,0 +1,35 @@
+import time
+
+"""
+  Homework#5
+
+  Add your name here: ----
+
+  You are free to create as many classes within the hw5.py file or across 
+  multiple files as you need. However, ensure that the hw5.py file is the 
+  only one that contains a __main__ method. This specific setup is crucial 
+  because your instructor will run the hw5.py file to execute and evaluate 
+  your work.
+"""
+
+if __name__ == "__main__": 
+    start_time = time.perf_counter()  # Do not remove this line
+    '''
+    Inisde the __main__, do not add any codes before this line.
+    -----------------------------------------------------------
+    '''
+
+    
+    # write your code here
+
+
+    '''
+    Inside the __main__, do not add any codes after this line.
+    ----------------------------------------------------------
+    '''
+    end_time = time.perf_counter()
+    # Calculate the runtime in milliseconds
+    runtime_ms = (end_time - start_time) * 1000
+    print(f"The runtime of the program is {runtime_ms} milliseconds.")  
+    
+
