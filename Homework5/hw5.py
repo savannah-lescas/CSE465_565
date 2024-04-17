7f1245f709d0f7939b25e54c3100c53a87baf674
savannah-lescas <lescas6502@gmail.com>
2024-04-16 20:04:13 -0400

    created the superclass and sublass

--- Homework5/hw5.py
+++ Homework5/hw5.py
@@ -3,7 +3,7 @@ import time
 """
   Homework#5
 
-  Add your name here: ----
+  Add your name here: Savannah Lescas
 
   You are free to create as many classes within the hw5.py file or across 
   multiple files as you need. However, ensure that the hw5.py file is the 
@@ -32,4 +32,61 @@ if __name__ == "__main__":
     runtime_ms = (end_time - start_time) * 1000
     print(f"The runtime of the program is {runtime_ms} milliseconds.")  
     
+iFile = "zipcodes.txt"
 
+zipcodeFile = open(iFile, "r")
+
+class SimplePlace:
+    # constructor
+    def __init__(self, city, zipcode):
+        self._city = city
+        self._zipcode = zipcode
+
+    # override equals at some point
+    
+    # getter for city
+    def get_city(self):
+        return self._city
+    
+    # setter for city
+    def set_city(self, value):
+        self._city = value
+
+    # getter for zipcode
+    def get_zip(self):
+        return self._zipcode
+    
+    # setter for zipcode
+    def set_zip(self, value):
+        self._zipcode = value
+
+class Place(SimplePlace):
+    def __intit__(self, city, zipcode, state, lat, lon):
+        SimplePlace.__init__(self, city, zipcode)
+        self._state = state
+        self._lat = lat
+        self._lon = lon
+
+    # getter for state
+    def get_state(self):
+      return self._state
+
+    # setter for state
+    def set_state(self, value):
+        self._state = value
+
+    # getter for latitude
+    def get_lat(self):
+        return self._lat
+
+    # setter for latitude
+    def set_lat(self, value):
+        self._lat = value
+
+    # getter for longitude
+    def get_lon(self):
+        return self._lon
+
+    # setter for longitude
+    def set_lon(self, value):
+        self._lon = value
