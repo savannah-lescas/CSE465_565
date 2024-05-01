8add3387d6f397a190fa9fe6f6e21af4effaf3fc
savannah-lescas <lescas6502@gmail.com>
2024-05-01 13:57:01 -0400

    Added all the tester files and started to work on the interpreter the way we learned how to in class today. Made an Interpreter class with attributes and constructor.

--- Homework6/Zpm.cpp
+++ Homework6/Zpm.cpp
@@ -8,9 +8,45 @@
 
 using namespace std;
 
+typedef pair<string, string> stringPair;
+typedef unordered_map<string, string> dictionary;
+
+class Interpreter {
+    public:
+        string filename;
+        dictionary variables; // made need to make the type generic for var
+        int lineNumber = 0;
+
+        // Define the token specification
+         vector<stringPair> TOKEN_SPECIFICATION = {
+            {"INT_VAR",     "[a-zA-Z_][a-zA-Z_0-9]*\\s"},                   // Integer variable (lookahead for assignment and operations)
+            {"STR_VAR",     "[a-zA-Z_][a-zA-Z_0-9]*\\s"},                   // String variable (lookahead for assignment and addition)
+            {"ASSIGN",      "(?<=\\s)\\=(?=\\s)"},                           // Assignment operator
+            {"PLUS_ASSIGN", "(?<=\\s)\\+=(?=\\s)"},                          // Addition assignment operator
+            {"MINUS_ASSIGN","(?<=\\s)-=(?=\\s)"},                            // Subtraction assignment operator
+            {"MULT_ASSIGN", "(?<=\\s)\\*=(?=\\s)"},                          // Multiplication assignment operator
+            {"INT_VAR_VAL", "(?<=\\+=|-=|\\*=)\\s[a-zA-Z_][a-zA-Z_0-9]*"},    // Integer variable (lookahead for assignment and operations)
+            {"STR_VAR_VAL", "(?<=\\+=)\\s[a-zA-Z_][a-zA-Z_0-9]*"},           // String variable (lookahead for assignment and addition)
+            {"NUMBER",      "(?<=\\s)-?\\d+(?=\\s)"},                         // Integer literal
+            {"STRING",      "\"[^\"]*\""},                                   // String literal, handling quotes
+            {"SEMICOLON",   "(?<=\\s);"},                                    // Statement terminator
+            {"WS",          "\\s+"},                                        // Whitespace
+            {"NEWLN",       "\\n"}
+        };
+
+        Interpreter(string file_name) {
+            filename = file_name;
+            variables = {};
+            lineNumber = 0;
+        }
+
+};
+
 //template<typename Value>
 //unordered_map<string, Value> assignments;
 int lineNumber = 0;
+
+
 
 int main (int argc, char *argv[]) {
     string filename = "";
@@ -33,6 +69,8 @@ int main (int argc, char *argv[]) {
         cout << "File could not be opened" << endl;
         exit(1);
     }
+    
+    Interpreter interpreter(filename);
 
     return (0);
 }
