3ddf95d29275b186bf258baff17b2e0d3d800bf2
savannah-lescas <lescas6502@gmail.com>
2024-05-08 18:14:02 -0400

    I think I am giving up on the tokenizing and stuff and going back to my Java Zpm program and trying to copy that

--- Homework6/Zpm.cpp
+++ Homework6/Zpm.cpp
@@ -8,154 +8,25 @@
 #include <regex>
 
 using namespace std;
-
-typedef pair<string, string> stringPair;
-
-string strip(string unStrippedString) {
-    string strippedString = "";
-    if (unStrippedString.size() >= 2 && unStrippedString.front() == '"' && unStrippedString.back() == '"') {
-        strippedString = unStrippedString.substr(1, unStrippedString.size() - 2);
-    } 
-    return strippedString;
-}
 template<typename T>
-class Interpreter {
-    public:
-        string filename;
-        unordered_map<string, T> variables; // made need to make the type generic for var
-        int lineNumber = 0;
+unordered_map<string, T> variables;
+int lineNumber = 0;
 
-        // Define the token specification
-        vector<stringPair> TOKEN_SPECIFICATION = {
-            {"INT_VAR",     "[a-zA-Z_][a-zA-Z_0-9]*\\s"},                   // Integer variable (lookahead for assignment and operations)
-            {"STR_VAR",     "[a-zA-Z_][a-zA-Z_0-9]*\\s"},                   // String variable (lookahead for assignment and addition)
-            {"ASSIGN",      "(\\s)=\\s"},                           // Assignment operator
-            {"PLUS_ASSIGN", "(\\s)\\+=\\s"},                          // Addition assignment operator
-            {"MINUS_ASSIGN","(\\s)-=\\s"},                            // Subtraction assignment operator
-            {"MULT_ASSIGN", "(\\s)\\*=\\s"},                          // Multiplication assignment operator
-            {"INT_VAR_VAL", "(\\+=|-=|\\*=)\\s[a-zA-Z_][a-zA-Z_0-9]*"},    // Integer variable (lookahead for assignment and operations)
-            {"STR_VAR_VAL", "(\\+=)\\s[a-zA-Z_][a-zA-Z_0-9]*"},           // String variable (lookahead for assignment and addition)
-            {"NUMBER",      "(\\s)-?\\d+\\s"},                         // Integer literal
-            {"STRING",      "\"[^\"]*\""},                                   // String literal, handling quotes
-            {"SEMICOLON",   "(\\s);"},                                    // Statement terminator
-            {"WS",          "\\s+"},                                        // Whitespace
-            {"NEWLN",       "\\n"}
-        };
+void doAssignments(vector<string> input) {
+    string firstInstruction = input[0];
 
-        Interpreter(string file_name) {
-            filename = file_name;
-            variables = {};
-            lineNumber = 0;
+    if (firstInstruction == "FOR") {
+        //loop(input);
+    } else if (firstInstruction == "PRINT") {
+        //print(input[1]);
+    } else {
+        try {
+            //instruction(input);
+        } catch (...) {
+            cout << "Error" << endl;
         }
-
-        
-        vector<stringPair> lexicalAnalysis(string line) {
-            vector<stringPair> tokens;
-            
-            // ChatGPT for loop and tok_type/tok_regex
-            for (const auto& pair : TOKEN_SPECIFICATION) { 
-                const std::string& tok_type = pair.first;
-                const std::string& tok_regex = pair.second;
-                
-                // keeps track of what has been read and what hasn't
-                int pos = 0;
-                cout << "in loop" << endl;
-                
-                // same as re.compile
-                regex regex(tok_regex);
-                /*
-                while (pos < line.length()) {
-                    // keeps track of the string to check
-                    // with pos as the position that gets updated
-                    // once something is read, the new string will
-                    // not include what is already read
-                    string substring = line.substr(pos);
-                    // smatch holds information about the regex_search call
-                    // can tell us about the substrings and positions
-                    smatch match;
-                    // using regex_search becasue regex_match gives true or false
-                    // when we need actual information
-                    if (regex_search(substring, match, regex)) { // ChatGpt for regex_search 
-                        if (tok_type != "ws" && tok_type != "NEWLN") {
-                            // retrieves the matches string
-                            string matchedToken = match.str(0);
-                            // adds the token type with the token to the token vector
-                            tokens.push_back({tok_type, matchedToken});
-                        }
-
-                        // updates position 
-                        pos += match.length();
-                        
-                        // this condition is to stop checking the rest of the line
-                        // once we find either an integer variable or string variable
-                        // and then , we start looking for the next pattern on the line
-                        if (tok_type == "INT_VAR" || tok_type == "STR_VAR") {
-                            break;
-                        }
-                    } else {
-                        pos += 1;
-                    }
-                }
-                */
-            }
-            
-            return tokens;
-        }
-
-        void parse(vector<stringPair> tokens) {
-            vector<stringPair>::iterator it;
-
-            for (it = tokens.begin(); it != tokens.end(); it++) {
-                if (it->first == "INT_VAR" || it->second == "STR_VAR") {
-                    string varName = it->second;
-                    it++;
-                    it++;
-                    string opToken = it->second;
-                    it++;
-                    stringPair valueToken(it->first, it->second);
-                    it++;
-                    string semicolon = it->second;
-
-                    auto value = T{};
-                    if (valueToken.first == "NUMBER") {
-                        value = stoi(valueToken.second);
-                    } else if (valueToken.first == "STRING") {
-                        value = strip(valueToken.second);
-                    } else {
-                        value = variables[valueToken.first];
-                    }
-
-                    try {
-                        if (opToken == "=") {
-                            variables[varName] = value;
-                        } else if (opToken == "+=") {
-                            variables[varName] += value;
-                        } /*else if (opToken == "-=") {
-                            variables[varName] -= value;
-                        } else if (opToken == "*="){
-                            variables[varName] *= value;
-                        }*/
-                    } catch (...) {
-                        cout << "Error in line: " << lineNumber << endl;
-                        exit(1);
-                    }
-                }
-            }
-        }
-
-        void run() {
-            lineNumber = 0;
-            string line;
-            ifstream inputFile(filename);
-            while (getline(inputFile, line)) {
-                lineNumber++;
-
-                vector<stringPair> tokens = lexicalAnalysis(line);
-                //parse(tokens);
-            }
-        }
-
-};
+    }
+}
 
 
 int main (int argc, char *argv[]) {
@@ -179,26 +50,6 @@ int main (int argc, char *argv[]) {
         cout << "File could not be opened" << endl;
         exit(1);
     }
-    
-    Interpreter<string> interpreter(filename); // Assuming string as the type for variables
-    
-    interpreter.run(); // This will parse the file and populate the variables map
-    
-    // Print variables and their values
-    /*
-    for (const auto& variable : interpreter.variables) {
-        cout << variable.first << " = ";
-        // Check if the value is an integer
-        try {
-            int intValue = stoi(variable.second);
-            cout << intValue;
-        } catch (const std::invalid_argument&) {
-            // If stoi throws an exception, the value is not an integer
-            cout << variable.second; // Assume it's a string
-        }
-        cout << endl;
-    }
-    */
 
     return (0);
 }
